1e62f7161a3ea2a6c94a321eee90de128ed4126b
yowl <scott.waye@hubse.com>
2022-01-08 09:33:35 -0500

    debug

--- src/coreclr/tools/aot/ILCompiler.LLVM/Compiler/DependencyAnalysis/LLVMReadyToRunGenericLookupFromTypeNode.cs
+++ src/coreclr/tools/aot/ILCompiler.LLVM/Compiler/DependencyAnalysis/LLVMReadyToRunGenericLookupFromTypeNode.cs
@@ -10,6 +10,11 @@ namespace ILCompiler.DependencyAnalysis
         public LLVMReadyToRunGenericLookupFromTypeNode(NodeFactory factory, ReadyToRunHelperId helperId, object target, TypeSystemEntity dictionaryOwner)
             : base(factory, helperId, target, dictionaryOwner)
         {
+            if (target.ToString() ==
+                "[S.P.CoreLib]System.Array`1+ArrayEnumerator<System.Collections.Generic.LowLevelDictionary`2+Entry<Internal.Runtime.TypeLoader.TypeLoaderEnvironment+LazyDictionaryContext,native int>>.System.Collections.IEnumerator.get_Current()")
+            {
+
+            }
         }
 
         public override ObjectData GetData(NodeFactory factory, bool relocsOnly)
