308050ff7fd5867e77838704f9fa4c7bda80b7de
yowl00 <scott.waye@hubse.com>
2024-06-10 17:23:55 +0100

    install wasmer

--- eng/pipelines/runtimelab/install-wasmer.ps1
+++ eng/pipelines/runtimelab/install-wasmer.ps1
@@ -1,5 +1,19 @@
+[CmdletBinding(PositionalBinding=$false)]
+param(
+    $InstallDir,
+    [switch]$CI
+)
+
+Set-Location -Path $InstallDir
+
 Invoke-WebRequest -Uri https://github.com/wasmerio/wasmer/releases/download/v3.3.0/wasmer-windows-amd64.tar.gz -OutFile wasmer-windows-amd64.tar.gz
 
 mkdir wasmer
 
 tar -xzf wasmer-windows-amd64.tar.gz -C wasmer
+
+if ($CI)
+{
+    Write-Host "Setting WASMER_EXECUTABLE to '$InstallDir/wasmer/bin/wasmer.exe'"
+    Write-Output "##vso[task.setvariable variable=WASMER_EXECUTABLE]$InstallDir/wasmer/bin/wasmer.exe"
+}
