4df178c34d4df40ab7fcbcbea3859230bb4c416e
yowl00 <scott.waye@hubse.com>
2024-06-23 02:35:41 -0500

    set CC/CXX

--- eng/pipelines/runtimelab/install-llvm.ps1
+++ eng/pipelines/runtimelab/install-llvm.ps1
@@ -51,6 +51,21 @@ else
     git clone https://github.com/llvm/llvm-project --branch $LlvmProjectTag $DepthOption
 }
 
+# Set the compiler for CI on non-Windows
+if ($CI -and !$IsWindows) {
+    $RepoDir = Split-path $PSScriptRoot | Split-Path | Split-Path
+    Write-Host $RepoDir
+
+    bash -c "build_arch=amd64 compiler=clang source $RepoDir/eng/common/native/init-compiler.sh && set | grep -e CC -e CXX" |
+      ForEach-Object {
+       # Split the "<name>=<value>" line into the variable's name and value.
+       $name, $value = $_ -split '=', 2
+       # Define it as a process-level environment variable in PowerShell.
+       Set-Content ENV:$name $value
+       Write-Host $name $value
+     }
+}
+
 # There is no [C/c]hecked LLVM config, so change to Debug
 foreach ($Config in $Configs | % { if ($_ -eq "Checked") { "Debug" } else { $_ } } | Select-Object -Unique)
 {
