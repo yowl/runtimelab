fada9d5b42eb95b0a94c19e4d514aebb05302ff4
yowl <scott.waye@hubse.com>
2025-05-19 21:55:07 -0500

    disable failing tests

--- src/coreclr/nativeaot/Runtime/wasm/PalRedhawkWasm.cpp
+++ src/coreclr/nativeaot/Runtime/wasm/PalRedhawkWasm.cpp
@@ -191,6 +191,182 @@ extern "C" int __cxa_thread_atexit(Dtor dtor, void* obj, void*)
 #endif // TARGET_WASI
 #endif // !FEATURE_WASM_MANAGED_THREADS
 
+// TODO-LLVM: For now, a copy of the single threaded implementation.
+#ifdef FEATURE_WASM_MANAGED_THREADS
+int __cxa_thread_atexit(void (*func)(), void *obj, void *dso_symbol)
+{}
+
+//
+// Note that we return the native stack bounds here, not shadow stack ones. Currently this functionality is mainly
+// used for RuntimeHelpers.TryEnsureSufficientExecutionStack, and we do use the native stack in codegen, so this
+// is an acceptable approximation.
+//
+extern "C" unsigned char __stack_low;
+extern "C" unsigned char __stack_high;
+void PalGetMaximumStackBounds_MultiThreadedWasm(void** ppStackLowOut, void** ppStackHighOut)
+{
+    // See https://github.com/emscripten-core/emscripten/pull/18057 and https://reviews.llvm.org/D135910.
+    unsigned char* pStackLow = &__stack_low;
+    unsigned char* pStackHigh = &__stack_high;
+
+    // Sanity check that we have the expected memory layout.
+    ASSERT((pStackHigh - pStackLow) >= 64 * 1024);
+    if (pStackLow >= pStackHigh)
+    {
+        PalPrintFatalError("\nFatal error. Unexpected stack layout.\n");
+        RhFailFast();
+    }
+
+    *ppStackLowOut = pStackLow;
+    *ppStackHighOut = pStackHigh;
+}
+
+#ifdef TARGET_WASI
+// No-op stubs that assume a single-threaded environment.
+int pthread_mutex_init(pthread_mutex_t *, const pthread_mutexattr_t *)
+{
+    return 0;
+}
+
+int pthread_mutexattr_init(pthread_mutexattr_t *)
+{
+    return 0;
+}
+
+int pthread_mutexattr_settype(pthread_mutexattr_t *, int)
+{
+    return 0;
+}
+
+int pthread_mutex_destroy(pthread_mutex_t *)
+{
+    return 0;
+}
+
+int pthread_mutexattr_destroy(pthread_mutexattr_t *)
+{
+    return 0;
+}
+
+int pthread_cond_init(pthread_cond_t *, const pthread_condattr_t *)
+{
+    return 0;
+}
+
+int pthread_cond_destroy(pthread_cond_t *)
+{
+    return 0;
+}
+
+int pthread_cond_wait(pthread_cond_t *, pthread_mutex_t *)
+{
+    return 0;
+}
+
+int pthread_cond_timedwait(pthread_cond_t *, pthread_mutex_t *, const struct timespec *)
+{
+    return 0;
+}
+
+int pthread_condattr_init(pthread_condattr_t *)
+{
+    return 0;
+}
+
+int pthread_mutex_lock(pthread_mutex_t *)
+{
+    return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *)
+{
+    return 0;
+}
+
+pthread_t pthread_self(void)
+{
+    return (pthread_t)0;
+}
+
+int pthread_equal(pthread_t, pthread_t)
+{
+    return 1; // only one thread
+}
+
+int pthread_attr_init(pthread_attr_t *)
+{
+    return 0;
+}
+
+int pthread_attr_destroy(pthread_attr_t *)
+{
+    return 0;
+}
+
+int pthread_condattr_destroy(pthread_condattr_t *)
+{
+    return 0;
+}
+
+int pthread_cond_broadcast(pthread_cond_t *)
+{
+    return 0;
+}
+
+int pthread_attr_setdetachstate(pthread_attr_t *, int)
+{
+    return 0;
+}
+
+using Dtor = void(*)(void*);
+
+// Due to a bug in the toolchain, we have to provide an implementation of thread-local destruction.
+// Since this is the single-threaded case, we simply delegate to the static destruction mechanism.
+// Reference: https://github.com/llvm/llvm-project/blob/main/libcxxabi/src/cxa_thread_atexit.cpp.
+//
+extern "C" int __cxa_thread_atexit(Dtor dtor, void* obj, void*)
+{
+    struct DtorList
+    {
+        Dtor dtor;
+        void* obj;
+        DtorList* next;
+    };
+
+    struct DtorsManager
+    {
+        DtorList* m_dtors = nullptr;
+
+        ~DtorsManager()
+        {
+            while (DtorList* head = m_dtors)
+            {
+                m_dtors = head->next;
+                head->dtor(head->obj);
+                free(head);
+            }
+        }
+    };
+
+    // The linked list of "thread-local" destructors to run.
+    static DtorsManager s_dtorsManager;
+
+    DtorList* head = static_cast<DtorList*>(malloc(sizeof(DtorList)));
+    if (head == nullptr)
+    {
+        return -1;
+    }
+
+    head->dtor = dtor;
+    head->obj = obj;
+    head->next = s_dtorsManager.m_dtors;
+    s_dtorsManager.m_dtors = head;
+
+    return 0;
+}
+#endif // TARGET_WASI
+#endif // FEATURE_WASM_MANAGED_THREADS
+
 // Recall that WASM's model is extremely simple: we have one linear memory, which can only be grown, in chunks
 // of 64K pages. Thus, "mmap"/"munmap" fundamentally cannot be faithfully recreated and the Unix emulators we
 // layer on top of (Emscripten/WASI libc) reflect this by not supporting the scenario. Fortunately, the current
